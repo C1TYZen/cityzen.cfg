f59f9fdd9e88a5982d279ff0fc73ed3cffd6a586
Arseny Vasiliev <avasiliev97@yandex.ru>
2023-09-05 20:28:45 +0300

    New colors less graphics
    Change colorcheme in nvim
    Remove arrows in tmux

--- nvim/init.lua
+++ nvim/init.lua
@@ -95,13 +95,22 @@ require('lazy').setup({
 			vim.api.nvim_set_var('polyglot_disabled', {'perl'})
 		end,
 	},
-	{ 'morhetz/gruvbox',
+	-- { 'morhetz/gruvbox',
+	-- 	lazy = false,
+	-- 	priority = 1000,
+	-- 	config = function()
+	-- 		vim.opt.background = 'dark'
+	-- 		vim.g.gruvbox_italic = 1
+	-- 		vim.cmd([[colorscheme gruvbox]])
+	-- 	end,
+	-- },
+	{ "ellisonleao/gruvbox.nvim",
 		lazy = false,
 		priority = 1000,
 		config = function()
 			vim.opt.background = 'dark'
 			vim.g.gruvbox_italic = 1
-			vim.cmd([[colorscheme gruvbox]])
+			-- vim.cmd([[colorscheme gruvbox]])
 		end,
 	},
 	{ 'nvim-telescope/telescope-fzf-native.nvim',
@@ -298,6 +307,72 @@ require('lazy').setup({
 		end,
 	}
 })
+
+local custom_palette = {
+	dark0_hard     = '#1d2021',
+	-- dark0       = '#282828',
+	dark0          = "#000000",
+	dark0_soft     = '#32302f',
+	dark1          = '#3c3836',
+	dark2          = '#504945',
+	dark3          = '#665c54',
+	dark4          = '#7c6f64',
+	light0_hard    = '#f9f5d7',
+	light0         = '#fbf1c7',
+	light0_soft    = '#f2e5bc',
+	light1         = '#ebdbb2',
+	light2         = '#d5c4a1',
+	light3         = '#bdae93',
+	light4         = '#a89984',
+	bright_red     = '#fb4934',
+	bright_green   = '#b8bb26',
+	bright_yellow  = '#fabd2f',
+	bright_blue    = '#83a598',
+	bright_purple  = '#d3869b',
+	bright_aqua    = '#8ec07c',
+	bright_orange  = '#fe8019',
+	neutral_red    = '#cc241d',
+	neutral_green  = '#98971a',
+	neutral_yellow = '#d79921',
+	neutral_blue   = '#458588',
+	neutral_purple = '#b16286',
+	neutral_aqua   = '#689d6a',
+	neutral_orange = '#d65d0e',
+	faded_red      = '#9d0006',
+	faded_green    = '#79740e',
+	faded_yellow   = '#b57614',
+	faded_blue     = '#076678',
+	faded_purple   = '#8f3f71',
+	faded_aqua     = '#427b58',
+	faded_orange   = '#af3a03',
+	gray           = "#928373",
+}
+
+-- setup must be called before loading the colorscheme
+-- Default options:
+require("gruvbox").setup({
+	undercurl = true,
+	underline = true,
+	bold = true,
+	italic = {
+		strings = true,
+		comments = true,
+		operators = false,
+		folds = true,
+	},
+	strikethrough = true,
+	invert_selection = false,
+	invert_signs = false,
+	invert_tabline = false,
+	invert_intend_guides = false,
+	inverse = true, -- invert background for search, diffs, statuslines and errors
+	contrast = "", -- can be "hard", "soft" or empty string
+	palette_overrides = custom_palette,
+	overrides = {},
+	dim_inactive = false,
+	transparent_mode = false,
+})
+vim.cmd("colorscheme gruvbox")
 
 vim.filetype.add({
 	extension = {
