dbf06b08d82e97da572c92fe740b2dc0d65e931b
Arseny Vasiliev <avasiliev97@yandex.ru>
2023-09-17 14:21:27 +0300

    Remove unnecessary nvim files

--- nvim/lua/plugins/packer_install.lua
+++
@@ -1,9 +0,0 @@
- -- Добавляем Packer как пакет в Neovim
-vim.cmd [[packadd packer.nvim]]
-
--- Используем данный коллбэк как список для плагинов
-return require('packer').startup(function()
-	use 'wbthomason/packer.nvim'
-	use 'morhetz/gruvbox'
-	use {'neoclide/coc.nvim', branch = 'release'}
-end)
